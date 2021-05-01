332ea86843d5e8204926a483e0db66246bff2d1e
rogersm92 <roger.serra92@gmail.com>
2021-05-01 21:12:40 +0200

    hello world

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController //annotation that simplifies the creation of RESTful web services.
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping //to be used as a RESTful endpoint, we need this annotation
+	public String helloWorld() {
+		return "Hello World! This is a Springboot App!";
 	}
 
 }
