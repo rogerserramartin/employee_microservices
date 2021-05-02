5c47d6c86de03d9efa6d65098252b09b26423345
rogersm92 <roger.serra92@gmail.com>
2021-05-02 11:02:38 +0200

    Employee model created

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.employee.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.List;
 
 @SpringBootApplication
 @RestController //annotation that simplifies the creation of RESTful web services.
@@ -14,8 +20,17 @@ public class DemoApplication {
 	}
 
 	@GetMapping //to be used as a RESTful endpoint, we need this annotation
-	public String helloWorld() {
-		return "Hello World! This is a Springboot App!";
+	public List<Employee> helloWorld() {
+		return List.of(
+				new Employee(
+						1L,
+						"Vincent",
+						"vincent@lambda.com",
+						LocalDate.of(1980, Month.JULY, 20),
+						Period.between(LocalDate.of(1980, Month.JULY, 20), LocalDate.now()).getYears()
+
+				)
+		);
 	}
 
 }
---
+++ demo/src/main/java/com/example/demo/employee/Employee.java
@@ -0,0 +1,90 @@
+package com.example.demo.employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dateOfBirth;
+    private Integer age;
+
+    public Employee() {
+    }
+
+    public Employee(Long id,
+                    String name,
+                    String email,
+                    LocalDate dateOfBirth,
+                    Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public Employee(String name,
+                    String email,
+                    LocalDate dateOfBirth,
+                    Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
+
